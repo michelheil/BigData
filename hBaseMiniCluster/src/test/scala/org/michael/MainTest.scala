2b330d5c51748022aeeff4f2f2c7069e0e195408
michelheil <49286214+michelheil@users.noreply.github.com>
2020-02-27 08:15:06 +0100

    Added more general HBase testing functionalities

--- hBaseMiniCluster/src/test/scala/org/michael/MainTest.scala
+++ hBaseMiniCluster/src/test/scala/org/michael/MainTest.scala
@@ -1,15 +1,18 @@
 package org.michael
 
-import org.apache.hadoop.hbase.HBaseTestingUtility
-import org.apache.hadoop.hbase.client.{Get, Put, Result}
+import org.apache.hadoop.hbase.{HBaseTestingUtility, TableName}
+import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.util.Bytes
 import org.scalatest.{BeforeAndAfterAll, FeatureSpec}
 
 class MainTest extends FeatureSpec with BeforeAndAfterAll {
+  val valueRaw = "data"
+  val rowName = "ROWKEY-1"
+
   var utility: HBaseTestingUtility = _
   val family: Array[Byte] = "CF".getBytes
   val qualifier: Array[Byte] = "CQ".getBytes
-  val value: Array[Byte] = "data".getBytes
+  val value: Array[Byte] = valueRaw.getBytes
 
   override def beforeAll(): Unit = {
     utility = new HBaseTestingUtility
@@ -22,26 +25,36 @@ class MainTest extends FeatureSpec with BeforeAndAfterAll {
     super.afterAll()
   }
 
-  feature("General HBase test using mini cluster") {
-    scenario(("creation of table, putting and getting row")) {
-      info("Ensure that only dfs and yarn is running.")
-      info("Hbase, local-master-backup and local-regionservers should not run.")
-      info("Scripts can be found at ~/GitHubRepositories/ToolCommands/hadoop")
 
-      val table = utility.createTable(Bytes.toBytes("myTestTable"), family)
-      // HBase version 2.2.3: createTable(TableName.valueOf("myTestTable"), family)
-      val putKey = new Put(Bytes.toBytes("ROWKEY-1"))
+  feature("General HBase test using mini cluster") {
+    scenario(("create table, putting row, getting row, check existence, delete table")) {
+
+      // Creating Table
+      val table = utility.createTable(TableName.valueOf("myTestTable"), family)
+
+      // Putting Row
+      val putKey = new Put(Bytes.toBytes(rowName))
       putKey.addColumn(family, qualifier, value)
       table.put(putKey)
-      //val obj = new HBaseTestObject()
-      //obj.setRowKey("ROWKEY-1")
-      //obj.setData1("DATA-1")
-      // MyHBaseDAO.insertRecord(table, obj)
-      val getKey = new Get(Bytes.toBytes("ROWKEY-1"))
+
+      // Getting Row
+      val getKey = new Get(Bytes.toBytes(rowName))
       getKey.addColumn(family, qualifier)
       val result: Result = table.get(getKey)
-      assertResult(Bytes.toString(result.getRow))("ROWKEY-1")
-      assertResult(Bytes.toString(result.value))("DATA-1")
-    }
-  }
+
+      assertResult(rowName)(Bytes.toString(result.getRow))
+      assertResult(valueRaw)(Bytes.toString(result.value))
+
+      // Check if table exists
+      val admin = utility.getAdmin()
+      val tablesExists = admin.tableExists(TableName.valueOf("myTestTable"))
+
+      // Assertions
+      assertResult(true)(tablesExists)
+
+      // delete table
+      admin.disableTable(TableName.valueOf("myTestTable"))
+      admin.deleteTable(TableName.valueOf("myTestTable"))
+      val tablesNotExists = admin.tableExists(TableName.valueOf("myTestTable"))
+      assertResult(false)(tablesNotExists)
 }
