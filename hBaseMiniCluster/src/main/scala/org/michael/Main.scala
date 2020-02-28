5b7a58066ba58fa12c5371965aa1c41e6ae68467
Michael <michelheil@gmx.de>
2020-02-28 17:19:21 +0100

    added basic commands using Scala API for HBase

--- hBaseMiniCluster/src/main/scala/org/michael/Main.scala
+++ hBaseMiniCluster/src/main/scala/org/michael/Main.scala
@@ -1,14 +1,24 @@
 package org.michael
 
+import java.util
+import collection.JavaConversions._
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
-import org.apache.hadoop.hbase.client.{Admin, Connection, ConnectionFactory, Get, Put}
+import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.util.Bytes
 
 /**
  * Play around with HBase
  */
 object Main extends App {
+
+  val tableNameString = "myFirstTable"
+  val tableName = TableName.valueOf(tableNameString)
+  val rowKey = "rowkey".getBytes
+  val colFamily = "colF".getBytes
+  val colQualifier = "id1".getBytes
+  val colValue = "one".getBytes
 
   // install hadoop and HBase as written in
   // https://computingforgeeks.com/how-to-install-apache-hadoop-hbase-on-ubuntu/
@@ -31,22 +41,52 @@ object Main extends App {
   println("Create Admin")
   val admin: Admin = connection.getAdmin()
 
-  println("List Tables")
-  val listTables = admin.listTables()
-  listTables.foreach(println)
+  println("Check if table exists:")
+  val tableNotYetExists: Boolean = admin.tableExists(tableName)
+  println(s"Table " + tableName.toString + " exists? " + tableNotYetExists)
 
-  val tableName = TableName.valueOf("myFirstTable")
-  val table = connection.getTable(tableName)
+  println("Create Table")
+  val colFamilyBuild = ColumnFamilyDescriptorBuilder.newBuilder(colFamily).build()
+  val tableDescriptorBuild = TableDescriptorBuilder.newBuilder(tableName).setColumnFamily(colFamilyBuild).build()
+  admin.createTable(tableDescriptorBuild)
 
-  val thePut: Put = new Put(Bytes.toBytes("rowkey1"))
+  println("Check if table exists:")
+  val tableExists: Boolean = admin.tableExists(tableName)
+  println(s"Table " + tableName.toString + " exists? " + tableExists)
 
-  thePut.addColumn(Bytes.toBytes("colF"),Bytes.toBytes("id1"),Bytes.toBytes("one"))
+  println("List Tables:")
+  val listTables2: util.List[TableDescriptor] = admin.listTableDescriptors
+  listTables2.foreach(htd => println(htd.getTableName)) // required: collection.JavaConversions._
+
+  println("Get a Table handle")
+  val table: Table = connection.getTable(tableName)
+
+  println("Write a row into table")
+  val thePut: Put = new Put(rowKey)
+  thePut.addColumn(colFamily, colQualifier, colValue)
   table.put(thePut)
 
-  val theGet: Get = new Get(Bytes.toBytes("rowkey1"))
+  println("Query a row out of table:")
+  val theGet: Get = new Get(rowKey)
   val result = table.get(theGet)
-  val value = result.value()
+  val value: Array[Byte] = result.value()
   println(Bytes.toString(value))
+
+  println("Scan table:")
+  val theScan = new Scan()
+  val scanResults: ResultScanner = table.getScanner(theScan)
+  for(scanResult <- scanResults) {println(Bytes.toString(scanResult.getValue(colFamily, colQualifier)))}
+  scanResults.close()
+
+  println("Delete table")
+  admin.disableTable(tableName)
+  admin.deleteTable(tableName)
+  println("Check if table exists:")
+  val tableNotExists: Boolean = admin.tableExists(tableName)
+  println(s"Table " + tableName.toString + " exists? " + tableNotExists)
+
+  println("Close table")
+  table.close()
 }
 
 
--- hBaseMiniCluster/src/test/scala/org/michael/GeneralHBaseMockTest.scala
+++ hBaseMiniCluster/src/test/scala/org/michael/GeneralHBaseMockTest.scala
@@ -5,7 +5,7 @@ import org.apache.hadoop.hbase.client._
 import org.apache.hadoop.hbase.util.Bytes
 import org.scalatest.{BeforeAndAfterAll, FeatureSpec}
 
-class MainTest extends FeatureSpec with BeforeAndAfterAll {
+class GeneralHBaseMockTest extends FeatureSpec with BeforeAndAfterAll {
   val valueRaw = "data"
   val rowName = "ROWKEY-1"
 
@@ -15,8 +15,8 @@ class MainTest extends FeatureSpec with BeforeAndAfterAll {
   val value: Array[Byte] = valueRaw.getBytes
 
   override def beforeAll(): Unit = {
-    utility = new HBaseTestingUtility
-    utility.startMiniCluster(1,3, true)
+    utility = new HBaseTestingUtility()
+    utility.startMiniCluster(1, 3, true)
     super.beforeAll()
   }
 
@@ -28,6 +28,9 @@ class MainTest extends FeatureSpec with BeforeAndAfterAll {
 
   feature("General HBase test using mini cluster") {
     scenario(("create table, putting row, getting row, check existence, delete table")) {
+      info("Ensure that only dfs and yarn is running.")
+      info("Hbase, local-master-backup and local-regionservers should not run.")
+      info("Scripts can be found at ~/GitHubRepositories/ToolCommands/hadoop")
 
       // Creating Table
       val table = utility.createTable(TableName.valueOf("myTestTable"), family)
@@ -57,4 +60,6 @@ class MainTest extends FeatureSpec with BeforeAndAfterAll {
       admin.deleteTable(TableName.valueOf("myTestTable"))
       val tablesNotExists = admin.tableExists(TableName.valueOf("myTestTable"))
       assertResult(false)(tablesNotExists)
+    }
+  }
 }
