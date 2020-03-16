5657a49e34787f35d8083be115002a7635b9cf0d
Michael <michelheil@gmx.de>
2020-03-16 17:56:29 +0100

    added prints for debugging

--- sparkPartitioning/src/main/scala/org/michael/big/data/spark/partitioning/Main.scala
+++ sparkPartitioning/src/main/scala/org/michael/big/data/spark/partitioning/Main.scala
@@ -15,20 +15,25 @@ object Main extends App {
 
   // create test data
   val endRange: Long = 10L
-  val numbers: RDD[Long] = spark.range(1, endRange, 1, 3).toDF().rdd.map(row => row.getLong(0))
+  val numbers: RDD[Long] = spark.range(0, endRange, 1, 3).toDF().rdd.map(row => row.getLong(0))
+  numbers.foreachPartition(it => it.foreach(zahl => println(zahl)))
 
   // create keys
-  val keyedNumbers: RDD[(Int, Long)] = numbers.keyBy(_ => 1 + Random.nextInt(3))
+  val keyedNumbers: RDD[(Int, Long)] = numbers.keyBy(_ => 1 + Random.nextInt(1))
   println("Test data:")
   for(number <- keyedNumbers) println(number)
 
   val startTimer: Long = System.currentTimeMillis()
 
-  // Calculate test a result
+  // Calculate result of Test A
   val zeroValue: (Long, Long) = (0L, 0L)
-  val rdd1 = keyedNumbers.aggregateByKey[(Long, Long)](zeroValue)(seqOp, combOp)
+  val rdd1: RDD[(Int, (Long, Long))] = keyedNumbers.aggregateByKey[(Long, Long)](zeroValue)(seqOp, combOp)
+  println("Print keyedNumbers as RDD")
+  rdd1.foreachPartition(it =>
+    it.foreach(tuple => println(tuple)
+  ))
 
-  val finalResults = rdd1.mapValues(v => v._1/v._2).collect()
+  val finalResults: Array[(Int, Double)] = rdd1.mapValues(v => (1.0*v._1)/v._2).collect()
   println("Average per Key:")
   for(finalResult <- finalResults) println(finalResult)
   /*
@@ -41,10 +46,16 @@ object Main extends App {
   */
 
   // Within-Partition Reduction Step
-  def seqOp(a: (Long, Long), b: Long): (Long, Long) = (a._1 + b, a._2 + 1)
+  def seqOp(a: (Long, Long), b: Long): (Long, Long) = {
+    println(s"""a._1: ${a._1}; b: $b; a._2: ${a._2}""")
+    (a._1 + b, a._2 + 1)
+  }
 
   // Cross-Partition Reduction Step
-  def combOp(a: (Long, Long), b: (Long, Long)): (Long, Long) = (a._1 + b._1, a._2 + b._2)
+  def combOp(a: (Long, Long), b: (Long, Long)): (Long, Long) = {
+    println(s"""a._1: ${a._1}; b._1: ${b._1}; a._2: ${a._2}; b._2: ${b._2}""")
+    (a._1 + b._1, a._2 + b._2)
+  }
 
 }
 
