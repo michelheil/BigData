422daec82008481ca6f54a197b3329d5734ee268
Michael <michelheil@gmx.de>
2020-04-13 16:48:43 +0200

    Examples for BasicProducer and BasicConsumer

---
+++ kafka/java/Consumer/src/main/java/org/michael/big/data/kafka/java/BasicConsumer.java
@@ -0,0 +1,39 @@
+package org.michael.big.data.kafka.java;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class BasicConsumer {
+    public static void main(String[] args) {
+        System.out.println("*** Starting Basic Consumer ***");
+
+        // define Topic to which records are produced to
+        final String topic = "hello-world-topic";
+
+        // Define Kafka Parameters
+        Properties settings = new Properties();
+        settings.put(ConsumerConfig.GROUP_ID_CONFIG, "basic-consumer");
+        settings.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        settings.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        settings.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        // consume messages with `poll` call and print out results
+        try(KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(settings)) {
+            consumer.subscribe(Arrays.asList(topic));
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+            }
+        }
+    }
+}
---
+++ kafka/java/Producer/src/main/java/org/michael/big/data/kafka/java/BasicProducer.java
@@ -0,0 +1,41 @@
+package org.michael.big.data.kafka.java;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class BasicProducer {
+
+    public static void main(String[] args) {
+        System.out.println("*** Starting Basic Producer ***");
+
+        // define Topic to which records are produced to
+        final String topic = "hello-world-topic";
+
+        // Define kafkaParameter
+        Properties settings = new Properties();
+        settings.put(ProducerConfig.CLIENT_ID_CONFIG, "basic-producer");
+        settings.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        settings.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // create KafkaProducer
+        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(settings);
+
+        // Add shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("### Stopping Basic Producer ###");
+            producer.close();
+        }));
+
+        // produce messages through `send` call
+        for(int i=1; i<=5; i++) {
+            final String key = "key-" + i;
+            final String value = "value-" + i;
+            final ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+            producer.send(record);
+        }
+    }
+}
